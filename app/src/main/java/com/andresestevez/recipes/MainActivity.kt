98ad03ff05ecd331ba7ab4ed8d4a7f67c41d0410
Baco <gangstandres@hotmail.com>
2021-07-26 21:20:45 +0200

    MainActivity & fragments
    ViewPager2, FragmentStateAdapter, TabLayout

--- app/src/main/java/com/andresestevez/recipes/MainActivity.kt
+++ app/src/main/java/com/andresestevez/recipes/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.andresestevez.recipes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.andresestevez.recipes.adapters.ViewPagerAdapter
+import com.andresestevez.recipes.databinding.ActivityMainBinding
+import com.andresestevez.recipes.fragments.FavFragment
+import com.andresestevez.recipes.fragments.LocalRecipesFragment
+import com.andresestevez.recipes.fragments.SearchFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragments = listOf<Fragment>(FavFragment(), SearchFragment(), LocalRecipesFragment())
+        binding.viewPager.adapter = ViewPagerAdapter(fragments, this)
+
+        val icons = listOf(
+            R.drawable.ic_baseline_favorite_24,
+            R.drawable.ic_baseline_search_24,
+            R.drawable.ic_baseline_location_searching_24
+        )
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.setIcon(icons[position])
+        }.attach()
     }
 }
