e1ca4674bc3c5e61ad722479614f3ec304be6645
Baco <andres.estevez.pro@gmail.com>
2021-08-19 17:51:41 +0200

    Favorite action in Detail

--- app/src/main/java/com/andresestevez/recipes/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/andresestevez/recipes/ui/detail/DetailActivity.kt
@@ -3,6 +3,7 @@ package com.andresestevez.recipes.ui.detail
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.andresestevez.recipes.R
 import com.andresestevez.recipes.databinding.ActivityDetailBinding
 import com.andresestevez.recipes.models.RecipesRepository
 import com.andresestevez.recipes.ui.common.app
@@ -23,6 +24,7 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.floatingBtn.setOnClickListener { viewModel.onFavoriteClicked() }
         viewModel.model.observe(this, {updateUI(it)})
 
         viewModel.refresh(intent.getStringExtra(EXTRA_RECIPE_ID))
@@ -32,13 +34,21 @@ class DetailActivity : AppCompatActivity() {
 
         when (model) {
             is DetailViewModel.UiModel.Content -> {
-                model.recipe?.let {
+                model.recipe.let {
                     with(binding) {
                         Glide.with(this@DetailActivity).load(it.thumbnail).into(imageView)
                         toolbar.title = it.name
                         ingredients.setIngredients(it)
                         instructions.text = it.instructions
+                        val favIcon = if (it.favorite) R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24
+                        floatingBtn.setImageDrawable(getDrawable(favIcon))
                     }
+                }
+            }
+            is DetailViewModel.UiModel.Favorite ->   {
+                model.recipe.let {
+                    val favIcon = if (it.favorite) R.drawable.ic_baseline_favorite_24 else R.drawable.ic_baseline_favorite_border_24
+                    binding.floatingBtn.setImageDrawable(getDrawable(favIcon))
                 }
             }
         }
