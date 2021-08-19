1ee29300cf3a8a26f9b2f65cb9a176f2143793c0
Baco <andres.estevez.pro@gmail.com>
2021-08-19 17:51:41 +0200

    Favorite action in Detail

--- app/src/main/java/com/andresestevez/recipes/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/andresestevez/recipes/ui/detail/DetailViewModel.kt
@@ -2,23 +2,34 @@ package com.andresestevez.recipes.ui.detail
 
 import androidx.lifecycle.*
 import com.andresestevez.recipes.models.RecipesRepository
+import com.andresestevez.recipes.models.database.Recipe
 import kotlinx.coroutines.launch
 
 class DetailViewModel(private val recipesRepository: RecipesRepository) : ViewModel() {
 
-    sealed class UiModel {
-        class Content(val recipe: com.andresestevez.recipes.models.database.Recipe?): UiModel()
+    sealed class UiModel(val recipe: Recipe) {
+        class Content(recipe: Recipe): UiModel(recipe)
+        class Favorite(recipe: Recipe): UiModel(recipe)
     }
 
     private val _model = MutableLiveData<UiModel>()
     val model : LiveData<UiModel>
         get() = _model
 
-
     fun refresh(recipeId: String?) {
         if (!recipeId.isNullOrEmpty()) {
             viewModelScope.launch {
                 _model.value = UiModel.Content(recipesRepository.findRecipeById(recipeId))
+            }
+        }
+    }
+
+    fun onFavoriteClicked() {
+        _model.value?.recipe?.let {
+            viewModelScope.launch {
+                val updatedRecipe = it.copy(favorite = !it.favorite)
+                recipesRepository.updateRecipe(updatedRecipe)
+                _model.value = UiModel.Favorite(updatedRecipe)
             }
         }
     }
@@ -30,5 +41,4 @@ class DetailViewModelFactory(private val recipesRepository: RecipesRepository) :
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return DetailViewModel(recipesRepository) as T
     }
-
 }
