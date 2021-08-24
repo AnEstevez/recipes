21dd990486baf283826a13be25fe95505790186b
Baco <andres.estevez.pro@gmail.com>
2021-08-24 11:11:03 +0200

    Implement Favorites fragment (close #21)

--- app/src/main/java/com/andresestevez/recipes/ui/main/fragments/FavFragment.kt
+++ app/src/main/java/com/andresestevez/recipes/ui/main/fragments/FavFragment.kt
@@ -5,56 +5,75 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.andresestevez.recipes.R
+import androidx.fragment.app.activityViewModels
+import com.andresestevez.recipes.databinding.FragmentFavBinding
+import com.andresestevez.recipes.models.RecipesRepository
+import com.andresestevez.recipes.ui.common.app
+import com.andresestevez.recipes.ui.common.startActivity
+import com.andresestevez.recipes.ui.detail.DetailActivity
+import com.andresestevez.recipes.ui.main.RecipesBDAdapter
+import com.andresestevez.recipes.ui.main.fragments.FavViewModel.UiModel.Loading
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FavFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FavFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentFavBinding? = null
+    private val binding
+        get() = _binding!!
+
+    private lateinit var adapter : RecipesBDAdapter
+
+    private val viewModel: FavViewModel by activityViewModels { FavFragmentFactory(RecipesRepository(requireActivity().app)) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fav, container, false)
+    ): View {
+
+        _binding = FragmentFavBinding.inflate(inflater, container, false)
+
+        initRecyclerView()
+        return binding.root    }
+
+    private fun initRecyclerView() {
+        adapter = RecipesBDAdapter {viewModel.onRecipeClicked(it.idMeal)}
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FavFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FavFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.recycler.adapter = adapter
+
+        viewModel.model.observe(viewLifecycleOwner, {
+            updateUi(it)
+        })
+
+        viewModel.navigation.observe(viewLifecycleOwner, {
+            it.getContentIfNotHandled().let {
+                activity?.startActivity<DetailActivity> {
+                    putExtra(DetailActivity.EXTRA_RECIPE_ID, it)
                 }
             }
+        })
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.refresh()
+    }
+
+    private fun updateUi(model: FavViewModel.UiModel) {
+        binding.progress.visibility = if (model == Loading) View.VISIBLE else View.GONE
+
+        when(model) {
+            is FavViewModel.UiModel.Content -> adapter.submitList(model.recipes)
+            else -> {}
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/andresestevez/recipes/ui/main/fragments/FavViewModel.kt
@@ -0,0 +1,42 @@
+package com.andresestevez.recipes.ui.main.fragments
+
+import androidx.lifecycle.*
+import com.andresestevez.recipes.models.RecipesRepository
+import com.andresestevez.recipes.models.database.Recipe
+import com.andresestevez.recipes.ui.common.Event
+import kotlinx.coroutines.launch
+
+class FavViewModel(private val repository: RecipesRepository): ViewModel() {
+
+    sealed class UiModel {
+        object Loading: UiModel()
+        class Content(val recipes: List<Recipe>) : UiModel()
+    }
+
+    private var _model = MutableLiveData<UiModel>()
+    val model : LiveData<UiModel>
+        get() = _model
+
+    private var _navigation = MutableLiveData<Event<String>>()
+    val navigation : LiveData<Event<String>>
+        get() = _navigation
+
+    fun refresh() {
+        viewModelScope.launch {
+            _model.value = UiModel.Loading
+            _model.value = UiModel.Content(repository.listFavorites())
+        }
+    }
+
+    fun onRecipeClicked(recipeId: String) {
+        _navigation.value = Event(recipeId)
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+class FavFragmentFactory(private val repository: RecipesRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return FavViewModel(repository) as T
+    }
+
+}
