d78ea039c7e0468da15f5faf99c90b89d0755077
Baco <andres.estevez.pro@gmail.com>
2021-10-08 14:21:11 +0200

    Unit tests for data and usecases modules

---
+++ usecases/src/test/java/com/andresestevez/usecases/GetRecipesByNameTest.kt
@@ -0,0 +1,45 @@
+package com.andresestevez.usecases
+
+import com.andresestevez.data.repository.RecipesRepository
+import com.andresestevez.data.repository.mockedRecipe
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+@RunWith(MockitoJUnitRunner::class)
+class GetRecipesByNameTest {
+
+    @Mock
+    lateinit var recipesRepository: RecipesRepository
+
+    private lateinit var getRecipesByName: GetRecipesByName
+
+    @Before
+    fun setUp() {
+        getRecipesByName = GetRecipesByName(recipesRepository)
+    }
+
+    @Test
+    fun `invoke calls recipesRepository`() {
+        runBlocking {
+            // GIVEN
+            val name = "carne"
+            val recipes = listOf(mockedRecipe.copy(id = "rec01", name = name))
+
+            whenever(recipesRepository.getRecipesByName(name)).thenReturn(recipes)
+
+            // WHEN
+            val result = getRecipesByName.invoke(name)
+
+            // THEN
+            verify(recipesRepository).getRecipesByName(name)
+            assertEquals(recipes, result)
+        }
+    }
+}
---
+++ usecases/src/test/java/com/andresestevez/usecases/ToggleRecipeFavoriteTest.kt
@@ -0,0 +1,76 @@
+package com.andresestevez.usecases
+
+import com.andresestevez.data.repository.RecipesRepository
+import com.andresestevez.data.repository.mockedRecipe
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.any
+import org.mockito.kotlin.verify
+
+@RunWith(MockitoJUnitRunner::class)
+class ToggleRecipeFavoriteTest {
+
+    @Mock
+    lateinit var recipesRepository: RecipesRepository
+
+    private lateinit var toggleRecipeFavorite: ToggleRecipeFavorite
+
+    @Before
+    fun setUp() {
+        toggleRecipeFavorite = ToggleRecipeFavorite(recipesRepository)
+    }
+
+    @Test
+    fun `invoke calls recipesRepository`() {
+        runBlocking {
+            // GIVEN
+            val recipe = mockedRecipe.copy(id = "rec02", favorite = false, dateModified = null)
+
+            // WHEN
+            val result = toggleRecipeFavorite.invoke(recipe)
+
+            // THEN
+            verify(recipesRepository).updateRecipe(any())
+            assertEquals(recipe.id, result.id)
+        }
+    }
+
+    @Test
+    fun `unfavorite recipe becomes favorite`() {
+        runBlocking {
+            // GIVEN
+            val recipe = mockedRecipe.copy(id = "rec02", favorite = false, dateModified = null)
+
+            // WHEN
+            val result = toggleRecipeFavorite.invoke(recipe)
+
+            // THEN
+            verify(recipesRepository).updateRecipe(any())
+            assertEquals(recipe.id, result.id)
+            assertTrue(result.favorite)
+            assertNotNull(result.dateModified)
+        }
+    }
+
+    @Test
+    fun `favorite recipe becomes unfavorite`() {
+        runBlocking {
+            // GIVEN
+            val recipe = mockedRecipe.copy(id = "rec02", favorite = true, dateModified = null)
+
+            // WHEN
+            val result = toggleRecipeFavorite.invoke(recipe)
+
+            // THEN
+            verify(recipesRepository).updateRecipe(any())
+            assertEquals(recipe.id, result.id)
+            assertFalse(result.favorite)
+            assertNotNull(result.dateModified)
+        }
+    }
+}
